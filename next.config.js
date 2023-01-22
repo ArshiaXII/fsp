a8bca7aa6649af92be88aa7472908de486cbed27
benanakkaya <ahmetbenanakkaya@gmail.com>
2023-01-22 17:37:03 +0300

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
   },
   images: {
     remotePatterns: [{protocol:'https',hostname:"st.myideasoft.com"},{protocol:'https',hostname:"st2.myideasoft.com"},{protocol:'https',hostname:"st3.myideasoft.com"},{protocol:'https',hostname:"*.myideasoft.com"},{protocol:"https",hostname:"img-itopya.mncdn.com"}],

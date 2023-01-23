fbb3478d3c66324d7a695c1251ad8c252d1e08a9
benanakkaya <ahmetbenanakkaya@gmail.com>
2023-01-23 15:07:59 +0300

    fix

--- src/app/account/layout.jsx
+++ src/app/account/layout.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+"use client"
+import { UsersContext } from '@/context/User';
+import { useRouter } from 'next/navigation';
+import React, { useContext, useEffect } from 'react';
 import SidebarMenu from './components/sidebarMenu';
 import * as S from "./styled"
 
 export default function AccountLayout({ children }) {
+
+
+    const router = useRouter();
+
+    const { isLoginned } = useContext(UsersContext);
+
+    useEffect(() => {
+        if (isLoginned === false) {
+            router.push('/login');
+        }
+    }, [isLoginned])
+
     return (
         <div className={S.mainDiv}>
             <div className={S.container}>
-                <SidebarMenu />
-                {children}
+                {isLoginned === true ?
+                    <>
+                        <SidebarMenu />
+                        {children}
+                    </>
+                    : null}
             </div>
         </div>
     )
--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,27 +1,13 @@
-"use client"
-import { UsersContext } from '@/context/User';
-import { useRouter } from 'next/navigation';
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import MainpageContent from './components/mainpageContent';
 
 export default function AccountPage() {
 
 
-  const router = useRouter();
-
-  const { isLoginned } = useContext(UsersContext);
-
-  useEffect(() => {
-    if (isLoginned === false) {
-      router.push('/login');
-    }
-  }, [isLoginned])
 
   return (
     <>
-      {isLoginned === true ?
-        <MainpageContent />
-        : null}
+      <MainpageContent />
     </>
   )
 }
